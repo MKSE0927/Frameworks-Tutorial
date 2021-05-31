0d85ab059c78da5fb3179e1340cdc904ee5b945f
mkse0927 <mkse0927@gmail.com>
2021-05-31 10:14:25 +0900

    Add prop Function

--- JavaScript/React/js/my-app/src/index.js
+++ JavaScript/React/js/my-app/src/index.js
@@ -4,7 +4,24 @@ import './index.css';
 
 // React.Componentを継承する。ReactのDOM定義を行う。
 class Square extends React.Component {
-    
+    /*
+        stateを利用して値更新を行う。setState()で値を設定できる。
+        今回は、クリック時に自身(Squre)のstate.valueに'X'を設定する。
+    */
+    render() {
+      return (
+        <button className="square" onClick={() => {this.props.onClick()}}>
+          {this.props.value}
+        </button>
+      );
+    }
+  }
+  
+  /**
+   * 升目(Square)の状態をstateで持ち、propsで描画する値が〇かXかを返す。
+   */
+  class Board extends React.Component {
+          
     /*  render()関数でReact要素を生成する。
         propsにある、「value」という名称の変数の値を利用する。
         propsは「親コンポーネントから子コンポーネント」に渡す。
@@ -12,30 +29,27 @@ class Square extends React.Component {
     constructor(props) {
         // JavScriptでのコンストラクタは常にsuper()の呼び出しが必要。
         super(props)
-        this.state = {
-            value:null,
+        this.state = {  
+            squares: Array(9).fill(null)
         };
     }
 
-    /*
-        stateを利用して値更新を行う。setState()で値を設定できる。
-        今回は、クリック時に自身(Squre)のstate.valueに'X'を設定する。
-    */
-    render() {
-      return (
-        <button className="square" onClick={() => {this.setState({value: 'X'})}}>
-          {this.state.value}
-        </button>
-      );
+    // 子コンポーネントに渡すクリック時の処理
+    handleClick(i) {
+        const squares = this.state.squares.slice()
+        squares[i] = 'X'
+        this.setState({squares:squares})
     }
-  }
   
-  class Board extends React.Component {
     // propsを利用したデータの受け渡し。Square関数にvalueという名称のpropsを渡す。
     renderSquare(i) {
-      return <Square value = {i} />;
+      return (
+            <Square
+                value = {this.state.squares[i]} 
+                onClick={() => this.handleClick(i)} />
+            );
     }
-  
+
     render() {
       const status = 'Next player: X';
   
